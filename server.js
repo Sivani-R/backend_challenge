c2797687e1b3a856d60caebb582d972fafb10ffa
sivaniramachandran85@gmail.com <sivaniramachandran85@gmail.com>
2024-10-27 16:51:55 +0530

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/productRoutes');
@@ -6,16 +5,13 @@ const productRoutes = require('./routes/productRoutes');
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use('/api/products', productRoutes);
 
-// Default route to handle root URL
 app.get('/', (req, res) => {
     res.send('Welcome to the Products API');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
